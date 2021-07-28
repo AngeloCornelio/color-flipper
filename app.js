875fa4f2726f407cccc2d68f3634c9ce9ba2d6ed
Angelo Cornelio <cornelio.angelo@gmail.com>
2021-07-28 13:02:41 -0700

    implement simple random color switching

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-alert("Connection established.");
+let colors = [
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "indigo",
+  "violet"
+];
+
+let colorText = document.getElementById("color");
+let body = document.querySelector("body");
+let button = document.querySelector("button");
+button.addEventListener('click', () => {
+  let randInt = Math.floor(Math.random() * colors.length);
+  body.style.backgroundColor = colors[randInt];
+  button.style.backgroundColor = colors[randInt];
+  colorText.innerText = colors[randInt];
+});
