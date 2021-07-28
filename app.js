838cfb117d62cc63c57514aaec372a06353743cd
Angelo Cornelio <cornelio.angelo@gmail.com>
2021-07-28 13:48:35 -0700

    implement togglable simple and hex mode buttons

--- app.js
+++ app.js
@@ -8,12 +8,28 @@ let colors = [
   "violet"
 ];
 
+let isSimpleMode = true;
+let simple = document.getElementById("simple");
+let hex = document.getElementById("hex");
 let colorText = document.getElementById("color");
 let body = document.querySelector("body");
 let button = document.querySelector("button");
+
 button.addEventListener('click', () => {
   let randInt = Math.floor(Math.random() * colors.length);
   body.style.backgroundColor = colors[randInt];
   button.style.backgroundColor = colors[randInt];
   colorText.innerText = colors[randInt];
 });
+
+simple.addEventListener('click', () => {
+  simple.className = "selected";
+  hex.className = "";
+  isSimpleMode = true;
+});
+
+hex.addEventListener('click', () => {
+  hex.className = "selected";
+  simple.className = "";
+  isSimpleMode = false;
+})
